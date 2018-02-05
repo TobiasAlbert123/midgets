1936abcaa4314d713981e1fa53e866169cd0f6da
TobiasAlbert123 <tobia17@student.uia.no>
2018-02-05 16:56:50 +0100

    loop oppgave 3 test

---
+++ Obligatorisk1/loop.go
@@ -0,0 +1,15 @@
+package algorithms
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	for sig := range c {
+		fmt.Println(sig)
+	}
+}
