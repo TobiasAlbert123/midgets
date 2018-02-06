dcbcf8d347bd7525a709e317b56fc176e03f7acf
TobiasAlbert123 <tobiasalbert97@gmail.com>
2018-02-06 14:54:47 +0100

    Ascii complete

--- Obligatorisk1/ascii/ascii.go
+++ Obligatorisk1/ascii/ascii.go
@@ -1,8 +1,17 @@
-package ascii
+package main
 
-func IterateOverASCIIStringLiteral(sl string) {
-	// Kode for Oppgave 2a
+import (
+	"fmt"
+)
+
+func main() {
+	a:= ""
+	b:= "\\x"
+	for i:=128; i < 256; i++ {
+		h := fmt.Sprintf("%X", i)
+		st := string(i)
+		a = a + b + h
+		fmt.Printf("%s %s %d\n", h, st, i)
+	}
+	fmt.Println(a)
 }
-
-// Kode for Oppgave 2b
-func ExtendedASCIIText() {}
