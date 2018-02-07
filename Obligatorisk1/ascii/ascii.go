d5baadf4aa8c220a44bb785b9dc3b16c4b027e98
TobiasAlbert123 <tobiasalbert97@gmail.com>
2018-02-07 14:38:40 +0100

    Ascii update

--- Obligatorisk1/ascii/ascii.go
+++ Obligatorisk1/ascii/ascii.go
@@ -5,6 +5,11 @@ import (
 )
 
 func main() {
+	//IterateOverASCIIStringLiteral()
+	ExtendedASCIIText()
+}
+
+func IterateOverASCIIStringLiteral() {
 	a:= ""
 	b:= "\\x"
 	for i:=128; i < 256; i++ {
@@ -14,11 +19,20 @@ func main() {
 		fmt.Printf("%s %s %d\n", h, st, i)
 	}
 	fmt.Println(a)
-}
-
-func IterateOverASCIIStringLiteral(sl string) {
 
 }
 
 func ExtendedASCIIText() {
+	a := string(34)
+	b := string(32)
+	c := string(8364)
+	d := string(247)
+	e := string(190)
+	f := string(100)
+	g := string(111)
+	h := string(108)
+	i := string(97)
+	j := string(114)
+	zz := a+b+c+b+d+b+e+b+f+g+h+h+i+j+b+a
+	fmt.Printf("%s", zz)
 }
