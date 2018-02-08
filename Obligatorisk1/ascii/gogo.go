30e3662ba2d600c3cd5e9deaf2e13ea71c061b78
TobiasAlbert123 <tobiasalbert97@gmail.com>
2018-02-08 08:46:22 +0100

    Ascii update, iso_test (gogo is temp name for iso_test)

--- Obligatorisk1/ascii/gogo.go
+++ Obligatorisk1/ascii/gogo.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"strings"
-	"strconv"
 )
 
 func main() {
@@ -11,14 +10,25 @@ func main() {
 }
 
 func test() {
-	fmt.Println("\" € ÷ ¾ dollar \"")
-	fmt.Printf("%q\n", strings.Split("\" € ÷ ¾ dollar \"", ""))
+	//fmt.Println("\" € ÷ ¾ dollar \"")
+	//fmt.Printf("%q\n", strings.Split("\" € ÷ ¾ dollar \"", ""))
+	riktig := 0
 	a := []string(strings.Split(" € ÷ ¾ dollar ", ""))
-	for i:=0; i < len(a); i++ {
+	notfound := false
+	for i:=0; i < len(a) && notfound == false; i++ {
 		b := a[i]
-		c := strconv.Atoi(b)
-		fmt.Println(c)
-		fmt.Printf("%s\n", a[i])
+		for t := 0; t < 256; t++{
+			if b == string(t){
+				riktig++
+				notfound = true
+			}
+		}
+		//fmt.Printf("%s\n", a[i])
+	}
+	fmt.Println(riktig)
+	if riktig/len(a) != 1 {
+		fmt.Println("Et symbol er ikke fra ascii listen")
 	}
 	fmt.Println(a)
+	fmt.Println(notfound)
 }
