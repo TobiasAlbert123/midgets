300e3108e565b5bc6536fd118dabed95c3f9537c
TobiasAlbert123 <tobia17@student.uia.no>
2018-02-13 19:53:30 +0100

    Some comments added, trying to test 4C. Code should work in playground

--- Obligatorisk1/ascii/iso_test.go
+++ Obligatorisk1/ascii/iso_test.go
@@ -6,9 +6,22 @@ import (
 )
 
 func main() {
-	test()
+	iso("\" € ÷ ¾ dollar \"") //"\" € ÷ ¾ dollar \"", ""
 }
 
-func test() {
-	fmt.Printf("%q\n", strings.Split(" € ÷ ¾ dollar ", ","))
+func iso(input string) {
+	//splits input into a string array
+	splitString := []string(strings.Split(input, ""))
+	//takes each character and converts to rune, then converts to int
+	for i := 0; i < len(splitString); i++ {
+		character := splitString[i]
+		CharToRune := []rune(character)
+		RuneToInt := int(CharToRune[0])
+		//prints whether its part of ASCII extended or not
+		if RuneToInt >= 0x80 && RuneToInt <= 0xFF {
+			fmt.Printf("%s is in extended ASCII table at position %d\n", character, RuneToInt)
+		} else {
+			fmt.Printf("%s is NOT in extended ASCII table (position %d)\n", character, RuneToInt)
+		}
+	}
 }
